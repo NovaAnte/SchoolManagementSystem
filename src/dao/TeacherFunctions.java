a66b9c4a295f123750dc448591ed18f59330eed3
Ola Lindgård <ola.lindgard@iths.se>
2021-05-11 10:50:24 +0200

    Ongoing work with TeacheDAO and TeacherFunctions...

--- src/dao/TeacherFunctions.java
+++ src/dao/TeacherFunctions.java
@@ -1,4 +1,6 @@
 package dao;
+import java.util.InputMismatchException;
+import static schoolmanagementsystem.SchoolManagementSystem.sc;
 
 public class TeacherFunctions {
     static TeacherDao teacherDao = new TeacherDao();
@@ -7,19 +9,87 @@ public class TeacherFunctions {
     }
 
     public static void showAllTeachers() {
-//        teacherDao.showAllTeachers();
+        teacherDao.showAllTeachers();
     }
 
     public static void showTeacher() {
-//        teacherDao.showTeacher();
+        
+        System.out.println("ID:");
+        int id = sc.nextInt();
+        sc.nextLine();
+        
+        teacherDao.showTeacher(id);
     }
 
     public static void updateTeacher() {
-//        teacherDao.updateTeacher();
+        
+        System.out.println("ID:");
+        int id = sc.nextInt();
+        sc.nextLine();
+        teacherDao.showTeacher(id);
+        
+        System.out.println("Press enter to leave field unchanged.");
+        System.out.println("Name:");
+        String name = sc.nextLine();
+        System.out.println("Press enter to leave field unchanged.");
+        System.out.println("Gender:");
+        String gender = sc.nextLine();
+        System.out.println("Press enter to leave field unchanged.");
+        System.out.println("Salary:");
+        double salary = sc.nextDouble();
+        sc.nextLine();
+        System.out.println("Press '0' (zero) to leave field unchanged.");
+        System.out.println("Age:");
+        int age = getInt(); // Calling getInt() to verify integer...
+        sc.nextLine();
+        System.out.println("Press enter to leave field unchanged.");
+        System.out.println("Email:");
+        String email = sc.nextLine();
+        
+        teacherDao.updateTeacher(id, name, gender, salary, age, email);
     }
 
     public static void addTeacher() {
-//        teacherDao.addTeacher();
+        
+        System.out.println("Name:");
+        String name = sc.nextLine();
+        System.out.println("Gender:");
+        String gender = sc.nextLine();
+        System.out.println("Salary:");
+        double salary = sc.nextDouble();
+        sc.nextLine();
+        System.out.println("Age:");
+        int age = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Email:");
+        String email = sc.nextLine();
+    
+        
+        teacherDao.addTeacher(name, gender, salary, age, email);
     }
     
+    
+    private static int getInt() {
+        
+        boolean loop = true;
+        int value = 0;
+        
+        while(loop) {
+            
+            try {
+                
+                value = sc.nextInt();
+                sc.nextLine();
+                loop = false;
+                
+            }
+            catch(InputMismatchException e) {
+                
+                System.out.println("Only integers!");
+                sc.nextLine();  
+            } 
+        }
+        
+        return value;
+    }
 }
