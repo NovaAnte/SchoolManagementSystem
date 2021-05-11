310c3b077378e1bbc919d82e76696bd49c08079b
Johnny Olsen <johnny.olsen@iths.se>
2021-05-11 14:12:42 +0200

    Uppdaterat funktionalitet

--- src/dao/TeacherFunctions.java
+++ src/dao/TeacherFunctions.java
@@ -1,18 +1,19 @@
 package dao;
+
 import static schoolmanagementsystem.HelpFunctions.inputGender;
 import static schoolmanagementsystem.HelpFunctions.readDouble;
 import static schoolmanagementsystem.HelpFunctions.readInt;
 import static schoolmanagementsystem.SchoolManagementSystem.sc;
 
 public class TeacherFunctions {
+
     static TeacherDao teacherDao = new TeacherDao();
-    
-    
+
     public static void removeTeacher() {
-        
+
         System.out.print("ID:");
         int id = readInt();
-        
+
         teacherDao.removeTeacher(id);
     }
 
@@ -21,43 +22,45 @@ public class TeacherFunctions {
     }
 
     public static void showTeacher() {
-        
+
         System.out.print("ID:");
         int id = readInt();
-        
+
         teacherDao.showTeacher(id);
     }
 
     public static void updateTeacher() {
-        
+
         System.out.print("ID:");
         int id = readInt();
-        teacherDao.showTeacher(id);
-        
-        System.out.println("Press enter to leave field unchanged.");
-        System.out.print("Name:");
-        String name = sc.nextLine();
-        System.out.println("Press enter to leave field unchanged.");
-        System.out.print("Gender:");
-        String gender = inputGender();
-        System.out.println("Press enter to leave field unchanged.");
-        System.out.print("Salary:");
-        double salary = readDouble();
-        System.out.println("Press enter to leave field unchanged.");
-        System.out.print("Age:");
-        int age = readInt(); // Calling getInt() to verify integer...
-        System.out.println("Press enter to leave field unchanged.");
-        System.out.print("Email:");
-        String email = sc.nextLine();
-        
-        teacherDao.updateTeacher(id, name, gender, salary, age, email);
+        boolean x = teacherDao.showTeacher(id);
+
+        if (x) {
+            System.out.println("Press enter to leave field unchanged.");
+            System.out.print("Name:");
+            String name = sc.nextLine();
+            System.out.println("Press enter to leave field unchanged.");
+            System.out.println("Gender:");
+            String gender = inputGender();
+            System.out.println("Press enter to leave field unchanged.");
+            System.out.print("Salary:");
+            double salary = readDouble();
+            System.out.println("Press enter to leave field unchanged.");
+            System.out.print("Age:");
+            int age = readInt(); // Calling getInt() to verify integer...
+            System.out.println("Press enter to leave field unchanged.");
+            System.out.print("Email:");
+            String email = sc.nextLine();
+
+            teacherDao.updateTeacher(id, name, gender, salary, age, email);
+        }
     }
 
     public static void addTeacher() {
-        
+
         System.out.print("Name:");
         String name = sc.nextLine();
-        System.out.print("Gender:");
+        System.out.println("Gender:");
         String gender = inputGender();
         System.out.print("Salary:");
         double salary = readDouble();
@@ -65,10 +68,8 @@ public class TeacherFunctions {
         int age = readInt();
         System.out.print("Email:");
         String email = sc.nextLine();
-        
+
         teacherDao.addTeacher(name, gender, salary, age, email);
     }
-    
-    
 
 }
